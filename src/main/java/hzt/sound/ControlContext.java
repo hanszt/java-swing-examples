5aa2bafb120c6ae569e6a30a68df322e1807f9b9
Hans Zuidervaart <hanszuidervaart@hotmail.com>
2022-03-07 13:51:34 +0100

    - added java 1.3 sound samples

---
+++ src/main/java/hzt/sound/ControlContext.java
@@ -0,0 +1,14 @@
+package hzt.sound;
+/*
+ * Copyright 2002 Sun Microsystems, Inc. All rights reserved.
+ * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+
+/**
+ * The interface for the JavaSound tabs to open and close audio resources.
+ */
+public interface ControlContext {
+        public void open();
+        public void close();
+}
