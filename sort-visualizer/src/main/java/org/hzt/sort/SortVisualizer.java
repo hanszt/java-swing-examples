5229cf75f62cfa8547573524fe1765655c86a4ab
Hans Zuidervaart <hanszuidervaart@hotmail.com>
2026-03-19 17:09:27 +0100

    Add Java docs

--- sort-visualizer/src/main/java/org/hzt/sort/SortVisualizer.java
+++ sort-visualizer/src/main/java/org/hzt/sort/SortVisualizer.java
@@ -2,7 +2,7 @@ package org.hzt.sort;
 
 @FunctionalInterface
 public interface SortVisualizer {
-    void updateVisuals(int i, int j);
+    void updateVisuals(int pivot, int compare);
 
     enum ShuffleType {RANDOM, REVERSED, NEARLY_SORTED}
 }
--- sort-visualizer/src/main/java/org/hzt/sort/StatsQuerier.java
+++ sort-visualizer/src/main/java/org/hzt/sort/StatsQuerier.java
@@ -1,5 +1,6 @@
 package org.hzt.sort;
 
+/// An interface that defines the sort statistics that can be queried.
 public interface StatsQuerier {
     long comparisons();
     long swaps();
